64d88d50e9d6cc2c1a93746968e854124be87217
hbamru <hunter.paull@bamru.org>
2025-04-20 20:22:46 +0000

    Fix lint error

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const express = require('express')
 const csp = require('helmet-csp')
-const helmet = require("helmet")
+const helmet = require('helmet')
 
 const {middleware: cache} = require('./cache')
 const {getMeta} = require('./list')
